cd785e45e065caf0713762b64ac02e62a47c2044
William Grant-Buckley <ahoy@williambuckley.co.uk>
2015-03-17 13:13:52 +0000

    Fix namespacing

--- src/JsonableProvider.php
+++ src/JsonableProvider.php
@@ -2,20 +2,21 @@
 namespace Songbird\Package\Jsonable;
 
 use League\Container\ContainerInterface;
+use League\Route\RouteCollection;
 use Songbird\PackageProviderAbstract;
 
 class JsonableProvider extends PackageProviderAbstract
 {
     /**
-     * @param \League\Container\ContainerInterface       $app
-     * @param \Songbird\Package\Jsonable\RouteCollection $router
+     * @param \League\Container\ContainerInterface $app
+     * @param \League\Route\RouteCollection        $router
      */
     protected function registerRoutes(ContainerInterface $app, RouteCollection $router)
     {
         $router->addRoute(
             'GET',
-            '/{documentId:[a-zA-Z0-9_\-\/]*}.{format:[(json)]*}',
-            'Songbird\Package\Jsonable\Controller'
+            '/{documentId:[a-zA-Z0-9_\-\/]*}.json',
+            'Songbird\Package\Jsonable\Controller::handle'
         );
     }
 }
